07c5b27f2f1e4291d5b6fa23af80f1e8b3dc6e61
lizchk <107196142+lizchk@users.noreply.github.com>
2023-03-16 15:06:03 +0200

    Tools added

---
+++ 02lab_Yakovenko/Tools/ChineseSign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace KMA.Lab02.Yakovenko.Tools
+{ 
+    internal enum ChineseSign
+    {
+        Monkey,
+        Rooster,
+        Dog,
+        Pig,
+        Rat,
+        Ox,
+        Tiger,
+        Rabbit,
+        Dragon,
+        Snake,
+        Horse,
+        Goat
+    }
+}
---
+++ 02lab_Yakovenko/Tools/SunSign.cs
@@ -0,0 +1,18 @@
+﻿namespace _01lab_Yakovenko.Tools
+{
+    internal enum SunSign
+    {
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+        Capricorn,
+        Aquarius,
+        Pisces
+    }
+}
